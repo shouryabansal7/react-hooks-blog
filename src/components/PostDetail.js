7d4461e32bcc1a1633827cd27f43ac996945054f
Shourya Bansal <shouryab2001@gmail.com>
2021-11-04 15:36:06 +0530

    fifth commit : create post detail page

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -1,5 +1,34 @@
+import { useParams } from 'react-router-dom';
+import { firestore } from '../firebase';
+import { doc, getDoc } from 'firebase/firestore';
+import { useState, useEffect } from 'react';
+
 function PostDetail() {
-  return <div className="">PostDetail</div>;
+  const [post, setPost] = useState({});
+  const { postId } = useParams();
+
+  useEffect(() => {
+    async function getDocument() {
+      const docRef = doc(firestore, 'posts', postId);
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        console.log('Document data:', docSnap.data());
+        setPost(docSnap.data());
+      } else {
+        // doc.data() will be undefined in this case
+        console.log('No such document!');
+      }
+    }
+    getDocument();
+  }, []);
+
+  return (
+    <div className="post-detail">
+      <h1>{post.title}</h1>
+      <p>{post.content}</p>
+    </div>
+  );
 }
 
 export default PostDetail;

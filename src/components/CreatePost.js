7891fe5e159585af1dffdffcd28bfb0246e26ae1
Shourya Bansal <shouryab2001@gmail.com>
2021-11-04 13:47:11 +0530

    third commit : storing posts in firestore cloud

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,9 +1,11 @@
-import { useState } from 'react';
+import { firestore } from '../firebase';
+import { useFormInput } from '../hooks';
+import { collection, addDoc } from 'firebase/firestore';
 
 function CreatePost() {
-  const [title, setTitle] = useState();
-  const [subTitle, setSubTitle] = useState();
-  const [content, setContent] = useState();
+  const title = useFormInput('');
+  const subTitle = useFormInput('');
+  const content = useFormInput('');
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -11,6 +13,13 @@ function CreatePost() {
     console.log('title', title);
     console.log('subTitle', subTitle);
     console.log('content', content);
+
+    const docRef = addDoc(collection(firestore, 'posts'), {
+      title: title.value,
+      content: content.value,
+      subTitle: subTitle.value,
+      createdAt: new Date(),
+    });
   }
 
   return (
@@ -20,23 +29,17 @@ function CreatePost() {
       <form onSubmit={handleSubmit}>
         <div className="form-field">
           <label>Title</label>
-          <input value={title} onChange={(e) => setTitle(e.target.value)} />
+          <input {...title} />
         </div>
 
         <div className="form-field">
           <label>Sub Title</label>
-          <input
-            value={subTitle}
-            onChange={(e) => setSubTitle(e.target.value)}
-          />
+          <input {...subTitle} />
         </div>
 
         <div className="form-field">
           <label>Content</label>
-          <textarea
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          ></textarea>
+          <textarea {...content}></textarea>
         </div>
 
         <button className="create-post-btn">Create Post</button>
